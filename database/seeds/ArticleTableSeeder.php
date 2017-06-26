9e9f3c50f3027e9a0603d97a820e9f22915e0479
Satjopg <sss.jopg@me.com>
2017-06-27 00:58:41 +0900

    fix seed data

--- database/seeds/ArticleTableSeeder.php
+++ database/seeds/ArticleTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Article;
 
 class ArticleTableSeeder extends Seeder
 {
@@ -11,10 +12,20 @@ class ArticleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create('ja_JP');
+        
+		for($i=0; $i<10; $i++)
+		{
+			DB::table('article')->insert([
+				'article_id'=>$faker->numberBetween(100000, 1000000),
+				'article_title'=>$faker->sentence($nbWords = 3),
+			]);
+		}
+/*
 		DB::table('article')->insert([
 			'article_id' => 'Hello World',
 			'article_title' => 'conglaturations!! Have a Good Night!!'
 		]);
+*/
     }
 }
